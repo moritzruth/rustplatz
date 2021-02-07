65359c195ecef185f493ab8534dc2b45a586da74
Moritz Ruth <dev@moritzruth.de>
2021-02-07 18:23:28 +0100

    Update teams

--- src/components/EventsList.vue
+++ src/components/EventsList.vue
@@ -1,10 +1,13 @@
 <template>
-  <ul class="space-y-4">
-    <li v-for="(event, index) in events" :key="index">
-      <span class="font-bold text-2xl text-yellow-300">{{ event.date }}</span>
-      <div>{{ event.text }}</div>
-    </li>
-  </ul>
+  <template v-if="events.length !== 0">
+    <ul class="space-y-4">
+      <li v-for="(event, index) in events" :key="index">
+        <span class="font-bold text-2xl text-yellow-300">{{ event.date }}</span>
+        <div>{{ event.text }}</div>
+      </li>
+    </ul>
+  </template>
+  <span v-else class="italic">Keine</span>
 </template>
 
 <script>
