9274edec1f80afcfc4ad2ae2c98904238533101a
Moritz Ruth <dev@moritzruth.de>
2021-02-09 20:25:54 +0100

    Update eslint config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ import vue from "@vitejs/plugin-vue"
 export default {
   plugins: [vue()],
   optimizeDeps: {
-    exclude: ["node-fetch", "twitch", "twitch-auth"]
+    exclude: ["twitch", "twitch-auth"]
   }
 }
