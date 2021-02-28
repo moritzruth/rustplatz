ce3068d47419aa7459e5493bdd55135198b3065a
Moritz Ruth <dev@moritzruth.de>
2021-02-28 13:25:09 +0100

    Fix live data fetching again

--- src/App.vue
+++ src/App.vue
@@ -221,7 +221,7 @@
           const hour = new Date().getHours()
 
           // if (hour >= 17 || hour < 1) await fetchLive()
-          if (hour < 1) await fetchLive()
+          if (true) await fetchLive()
           else await setOfflineData()
         } else if (this.nextSeasonDate.getTime() <= Date.now()) await fetchLive()
         else setEmptyData()
